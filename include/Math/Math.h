82e8cf7c7ad3290cc6cd57b05927c4b84fb37764
Dormin <zyxster@gmail.com>
2015-03-21 10:32:27 +0100

    Included missing headers in Math.h.

--- include/Math/Math.h
+++ include/Math/Math.h
@@ -1,9 +1,11 @@
 #ifndef ADON_MATH_H
 #define ADON_MATH_H
 
+#include "Circle.h"
 #include "Line2.h"
 #include "Point2.h"
 #include "Ray2.h"
+#include "Rectangle.h"
 #include "Segment2.h"
 #include "Utilities.h"
 #include "Vector2.h"
