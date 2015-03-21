de7fd08cbac864dd850ba1eef712780f62660586
Dormin <zyxster@gmail.com>
2015-03-21 20:13:51 +0100

    Renamed Math.h to AdonMath.h to avoid collision with the standard library's math.h.

--- include/Adon.h
+++ include/Adon.h
@@ -4,7 +4,7 @@
 #include "ECS/ComponentManager.h"
 #include "ECS/EntityManager.h"
 #include "ECS/SystemManager.h"
-#include "Math/Math.h"
+#include "Math/AdonMath.h"
 #include "Messaging/Message.h"
 #include "Messaging/MessageDispatcher.h"
 #include "Application/Application.h"
